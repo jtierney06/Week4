de099941306669e2d5d806414809e179108b32cf
jtierney06 <jamespt06@icloud.com>
2024-02-28 14:31:15 -0500

    Update Digits.java

--- Digits.java
+++ Digits.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 public class Digits{
     public static void main(String[] args){
-        int num = 258;
+        int num;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        num = input.nextInt();
+        input.close();
         int ones = num % 10;
         int tens = num % 100 - ones;
         int hunds = num % 1000 - tens - ones;
